d293e2fcdfef70941c9189bd73ac7bef5a496092
CM-IV <cmathIV@protonmail.com>
2021-03-19 18:45:32 -0500

    modified the header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,28 +15,28 @@ export default class Header extends Preact.Component<any, any> {
             <nav class="navbar" role="navigation" aria-label="dropdown navigation">
                 <div class="navbar-item has-dropdown is-hoverable">
                     <a class="navbar-link">
-                    Docs
+                    Home
                     </a>
 
                 <div class="navbar-dropdown">
                     <a class="navbar-item">
-                    Overview
+                    Docs
                     </a>
                     <a class="navbar-item">
-                    Elements
+                    About
                     </a>
                     <a class="navbar-item">
                     Components
                     </a>
                     <hr class="navbar-divider"></hr>
                 <div class="navbar-item">
-                    Version 0.9.1
+                    Version 0.1
                         </div>
                     </div>
                 </div>
             </nav>
 
-        )
+        );
 
     }
 
