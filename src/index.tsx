5a5b41c57a62eed567529eec86b08bd0c3fa822e
CM-IV <cmathIV@protonmail.com>
2021-03-19 12:16:14 -0500

    cleaned up index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import { render } from 'preact';
 
-const App = () => (
-    <>
-        <nav class="navbar" role="navigation" aria-label="dropdown navigation">
+
+
+const Header = () => (
+
+    <nav class="navbar" role="navigation" aria-label="dropdown navigation">
             <div class="navbar-item has-dropdown is-hoverable">
                 <a class="navbar-link">
                 Docs
@@ -21,11 +23,16 @@ const App = () => (
                 <hr class="navbar-divider"></hr>
             <div class="navbar-item">
                 Version 0.9.1
-                    </div>
                 </div>
             </div>
-        </nav>
+        </div>
+    </nav>
 
+);
+
+const Body = () => (
+
+    <>
         <section>
             <div class="container">
                 <div class="tile is-ancestor">
@@ -47,6 +54,15 @@ const App = () => (
                                     <figure class="image is-4by3">
                                         <img src="https://bulma.io/images/placeholders/640x480.png"></img>
                                     </figure>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. 
+                                    Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                                    <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. 
+                                    Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. 
+                                    Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. 
+                                    Sed eget varius ligula, at volutpat tortor.</p>
+                                    <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. 
+                                    Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. 
+                                    Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
                                 </div>
                             </article>
                         </div>
@@ -56,18 +72,38 @@ const App = () => (
         </section>
 
         <br/>
-
-        <footer class="footer">
-            <div class="content has-text-centered">
-                <p>&copy; Copyright 2021</p>
-                <p>
-                <strong>Testing Preact</strong> by <a href="https://dogtowncs.netlify.com/">CMIV</a>. The source code is licensed
-                <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
-                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-                </p>
-            </div>
-        </footer>
     </>
+
 );
+
+const Footer = () => (
+
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>&copy; Copyright 2021</p>
+            <p>
+            <strong>Testing Preact</strong> by <a href="https://dogtowncs.netlify.com/">CMIV</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            </p>
+        </div>
+    </footer>
+
+);
+
+
+const App = () => (
+        
+    <div>
+        <Header/>
+
+        <Body/>
+
+        <Footer/>
+
+    </div>
+);
+
+
 
 render(<App />, document.getElementById('app'));
